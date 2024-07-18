137fe2e6ff04d10be39e2b1da8e1e6e612bcb382
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-19 02:12:07 +0700

    _

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+API_ID = os.environ["API_ID"]
+API_HASH = os.environ["API_HASH"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+TIKTOK_WEB = os.environ["TIKTOK_WEB"]
